d7d9e28d19e8d9b39779b60f1239833de6b96b32
Abhangipratigna <pratigna2004@gmail.com>
2024-12-02 17:31:13 +0530

    Add files via upload

---
+++ E-note book/content.php
@@ -0,0 +1,8 @@
+
+
+<div class="container bg-white shadow w-50 p-5 mt-5">
+    <h1>E-Note book</h1>
+    <hr class="w-25">
+    <a href="manager.php"><button type="button" class="btn-lg btn btn-dark  p-4">Welcome to python E-note<span class="bi bi-person"></span></button></a>
+  
+</div>                  
